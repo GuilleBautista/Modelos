e96cb345a465a02fe3d8f91a5e154345c3f65075
Guille <33132152+GuilleBautista@users.noreply.github.com>
2022-01-19 19:10:56 +0100

    First steps in Acceleo

---
+++ acceleo/Games/game1.py
@@ -0,0 +1,19 @@
+import classes
+import time
+
+#Declare every action, item, npc, trigger and room
+
+if __name__ == "__main__":
+	print("Starting game frase")
+	while True:
+		
+		#Trigger section		
+
+		inp = input().lower()
+		
+		#Check if 'inp' is an action and execute it		
+
+		print(inp)
+		time.sleep(0.1)
+
+
